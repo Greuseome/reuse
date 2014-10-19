41e03ae074ab330fcb68b23d39c71688dbc2da3f
ekmeyerson <elliotmeyerson@gmail.com>
2014-10-19 16:45:10 -0500

    New Visualizer

---
+++ NetViz.py
@@ -0,0 +1,63 @@
+# Visualizer for Reuse Nets
+
+import ReuseNetwork
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize(net):
+
+    CUTOFF = 0.2 # cutoff weight for displaying edges
+
+    plt.ion()
+    plt.clf()
+    G = nx.Graph()
+    pos = {} # positions of nodes
+    labels = {} # node labels
+    edge_labels = {}
+    colors = [] # node colors
+    edgecolors = [] #edge colors: the bluer posser, redder negger
+    nodes = []
+    nodesize = [] 
+    layer = [] # the layer each node is in
+    layer_size = [0] # the size of each layer
+    deepest_layer = 0
+
+    # set node positions
+    for x in range(net.numNodes):
+        G.add_node(x)
+        nodes.append(x)
+        currLayer = 0
+        for y in range(x):
+            if abs(net.edgeWeights[y,x]) > 0:
+                if layer[y] >= currLayer: currLayer = layer[y] + 1
+        layer.append(currLayer)
+        if currLayer > deepest_layer:
+            deepest_layer = currLayer
+            layer_size.append(1)
+        else:
+            layer_size[currLayer] += 1
+    
+    nodes_in_layer = [0]*len(layer_size)
+    for x in range(net.numNodes):
+        l = layer[x]
+        nodes_in_layer[l] += 1
+        #pos[x] = (nodes_in_layer[l]/(float(layer_size[l])+1),l)
+        pos[x] = (nodes_in_layer[l]/float(max(layer_size)),l)
+    
+    # add edges
+    for x in range(net.numNodes):
+        for y in range(net.numNodes):
+            if abs(net.edgeWeights[x,y]) > CUTOFF:
+                G.add_edge(x,y)
+                #edge_labels[x,y] = "{0:.1f}".format(net.edgeWeights[x,y])
+                edgecolors.append(net.edgeWeights[x,y])
+        
+    # actually draw everything
+    nx.draw(G,pos,node_color='purple',edge_color=edgecolors,
+        with_labels=False,edge_cmap=plt.cm.RdBu)  
+    nx.draw_networkx_edges(G,pos,edge_color=edgecolors,
+                        edge_cmap=plt.cm.RdBu,width=5)
+    #nx.draw_networkx_edge_labels(G,pos,edge_labels)
+ 
+    plt.draw()
