141ccbbf4ab41f944c896b5f834b3bec8b9eef2d
merky <mhollen@cs.utexas.edu>
2014-12-09 11:55:08 -0600

    environment setup script added

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ATARI images and roms
+ln -s /u/mhollen/sift/ale-assets/images images
+ln -s /u/mhollen/sift/ale-assets/roms roms
+
+# CONDOR binaries
+export PATH=/lusr/opt/condor/bin:$PATH
+
+
