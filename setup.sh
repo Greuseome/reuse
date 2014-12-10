c7867df843c69d9ac4d18a79ccf4a316557228d4
merky <mhollen@cs.utexas.edu>
2014-12-10 14:23:55 -0600

    various small changes making life slightly easier

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [[ ! -d ./pyenv ]]; then
+    if [[ "$(which virtualenv)" == "" ]]; then
+        easy_install virtual_env
+    fi
+    virtualenv pyenv
+    source ./pyenv/bin/activate
+    pip install -r requirements.txt
+fi
+
 # ATARI images and roms
-ln -s /u/mhollen/sift/ale-assets/images images
-ln -s /u/mhollen/sift/ale-assets/roms roms
+[[ ! -d ./images ]] && ln -s /u/mhollen/sift/ale-assets/images images
+[[ ! -d ./roms ]] && ln -s /u/mhollen/sift/ale-assets/roms roms
 
 # CONDOR binaries
 export PATH=/lusr/opt/condor/bin:$PATH
--- simulator_condor.py
+++ simulator_condor.py
@@ -1,23 +1,32 @@
-import sys, os, time
+import sys
+import os
+import time
+import signal
 import shutil
+import datetime
 import ConfigParser
 import GSP as NE
-import numpy as np
 import cPickle
-from uuid import uuid1
+import argparse
 
 from simulator import SimulatorJob
 
 def find_num_objects(game):
-    images_dir = os.path.join('/u/mhollen/sift/ale-assets/images', game)
+    images_dir = os.path.join('images', game)
+    if not os.path.exists(images_dir):
+        raise Exception("Error: cannot find Atari Game images in {}" \
+                        .format(images_dir))
     return len(os.listdir(images_dir))
 
+# keep this global for easy cleanup
+generation_dir = None
+all_sims = []
 
-def evolve_atari_network(settings_file):
+def evolve_atari_network(settings_file, label=None):
     # read settings file
     config = ConfigParser.ConfigParser()
     config.read(settings_file)
-    
+
     # load settings
     game = config.get('task','game')
     input_size = find_num_objects(game)
@@ -39,7 +48,6 @@ def evolve_atari_network(settings_file):
         for r in reuseFiles.split(','):
             reusables.append(cPickle.load(open(r,'r')))
 
-
     # init evolution or restart from checkpoint
     checkpoint = config.get('task','checkpoint')
     if len(checkpoint) > 0:
@@ -47,12 +55,26 @@ def evolve_atari_network(settings_file):
     else:
         ne = NE.GSP(input_size, output_size, reusables, config)
 
+    # define experiment folder
+    evolve_id = datetime.datetime.now().strftime('%Y%M%d-%H%M-%f')
+    if not label:
+        game_dir = os.path.join(os.getcwdu(), 'results', game, evolve_id)
+    else:
+        game_dir = os.path.join(os.getcwdu(), 'results', game, label, evolve_id)
 
-    evolve_id = str(uuid1())
-    game_dir = os.path.join(os.getcwdu(), 'results', game, evolve_id)
     if not os.path.exists(game_dir):
         os.makedirs(game_dir)
+
+    fitness_file = os.path.join(game_dir, 'fitness.history')
+    bestnet_file = os.path.join(game_dir, 'best_network.pkl')
+    ne_file      = os.path.join(game_dir, 'ne.pkl')
+
+    # copy settings into experiment folder
     shutil.copy(settings_file, os.path.join(game_dir,'settings.ini'))
+
+    # make'em readable by everyone -- for science!
+    for f in [settings_file, fitness_file, bestnet_file, ne_file, game_dir]:
+        os.chmod(f, 777)
 
     print '*'*40
     print "All files will be saved to:"
@@ -61,7 +83,7 @@ def evolve_atari_network(settings_file):
 
     best_fitness = -10000000
 
-    for generation in range(start_gen,num_generations):
+    for generation in range(start_gen, num_generations):
         curr_best_fitness = -10000000
 
         generation_dir = os.path.join(game_dir, 'gen-%04d' % generation)
@@ -107,8 +129,7 @@ def evolve_atari_network(settings_file):
             if fitness > best_fitness:
                 # save the best
                 best_fitness = fitness
-                os.rename(sim.netfile,
-                          os.path.join(game_dir,'best_network.pkl'))
+                os.rename(sim.netfile, bestnet_file)
             else:
                 # delete temp network file
                 os.remove(sim.netfile)
@@ -117,12 +138,12 @@ def evolve_atari_network(settings_file):
         shutil.rmtree(generation_dir, ignore_errors=True)
 
         # write current best fitness to file
-        with open(os.path.join(game_dir, 'fitness.history'),'a') as curve:
+        with open(fitness_file,'a') as curve:
             curve.write(str(curr_best_fitness)+',') 
 
         # save neuroevolution progress       
-        with open(os.path.join(game_dir,'ne.pkl'),'wb') as world_state:
-            cPickle.dump(ne,world_state,2)
+        with open(ne_file, 'wb') as world_state:
+            cPickle.dump(ne, world_state, 2)
 
         print "+ gen: {}, best: {}, Best: {}".format(generation, 
                                                      curr_best_fitness,
@@ -132,12 +153,29 @@ def evolve_atari_network(settings_file):
         print "evolving..."
         ne.nextGen()
 
+    # write newline for easy formatting later
+    with open(os.path.join(game_dir, 'fitness.history'),'a') as curve:
+        curve.write('\n') 
+
     print "Generation "+str(generation)+", task complete."
 
 
+def exit_gracefully(signal, frame):
+    print "exiting prematurely!..."
+    # delete generation info
+    shutil.rmtree(generation_dir, ignore_errors=True)
+    sys.exit(0)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        evolve_atari_network(sys.argv[1])
-    else: raise Exception("usage: python simulator_test_condor.py [settings_file] ")
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--label', help="label for experiment")
+    parser.add_argument('-o', '--outdir', help="root output results directory")
+    parser.add_argument("settings_file", help="settings file for experiment")
+    args = parser.parse_args()
+
+    evolve_atari_network(args.settings_file, label=args.label)
 
 
